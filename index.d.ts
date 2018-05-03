3c7b4e174beea17b656fdc690b38b497b4b140c2
ukstv <sergey.ukustov@machinomy.com>
2018-05-03 09:26:56 +0300

    Add safe-buffer and ethereumjs-wallet as dependencies

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
-import { Buffer } from "safe-buffer";
+import { Buffer } from 'safe-buffer'
 
-declare module "ethereumjs-wallet" {
+declare module 'ethereumjs-wallet' {
 
   class Wallet {
-    static fromPrivateKey(key: Buffer): Wallet;
-    static fromV3(json: string, password: string): Wallet;
-    getPrivateKey(): Buffer;
-    getAddressString(): string;
+    static fromPrivateKey(key: Buffer): Wallet
+    static fromV3(json: string, password: string): Wallet
+    getPrivateKey(): Buffer
+    getAddressString(): string
   }
 
   namespace Wallet {}
 
-  export = Wallet;
+  export = Wallet
 }
 
-declare module "ethereumjs-wallet/hdkey" {
-  import * as Wallet from "ethereumjs-wallet"
+declare module 'ethereumjs-wallet/hdkey' {
+  import * as Wallet from 'ethereumjs-wallet'
 
   class EthereumHDKey {
     getWallet(): Wallet
